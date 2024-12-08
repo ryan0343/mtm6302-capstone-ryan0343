6248dd0e4be50c66bbeacb949f57d0da57590a9c
Robert Liam Ryan <robliryan0@gmail.com>
2024-12-07 23:28:45 -0500

    adds LS checking for correct and wrong answers, and function to render difficulty options

--- script.js
+++ script.js
@@ -5,9 +5,21 @@
 // Array: stored question
 // array consisting of many of the same components of a regular array, saved to a local placeholder.
 // Used to display the current question.
+// const questionStored = [
+//     id: "",
+
+// ]
 
 // Array: historyArr
 // array that stores completed questions inside localStorage, deleting if reset by the user.
+const historyArr = [
+    {
+        category: "games",
+        difficulty: "impossible",
+        favourite: false,
+        correct: false
+    }
+]
 
 // Array difficultyArr
 // stores all possible difficulties
@@ -17,15 +29,39 @@ const difficultyArr = [
     "Hard"
 ]
 
-// const difficultySelect
-// retrieves difficulty Element
+// const mainCard
+// retrieves location of mainCard element
+const mainCard = document.getElementById('mainCard');
 
-//
+// retrieve locally stored data if they exist
+const historyCorrectLS = JSON.parse(localStorage.getItem('historyCorrect'));
+const historyWrongLS = JSON.parse(localStorage.getItem('historyWrong'));
+
+// variables that track the number of correct and incorrect answers if locally stored. If they are not locally stored, then set them to 0.
+// const historyCorrect
+let historyCorrect = historyCorrectLS || 0;
+// const historyWrong
+let historyWrong = historyWrongLS || 0;
 
 // FUNCTIONS
 
 // renderDifficulties()
 // render the difficulty panel out using the items in difficultyArr
+function renderDifficulties(){
+    let mainCardHTML = `<h2 class="fw-bold text-white text-center">Select a Difficulty</h2> <div class="form-group bg-secondary fw-bold py-3 rounded-32 d-flex flex-column justify-content-around">`;
+
+    difficultyArr.forEach(difficulty => {
+        mainCardHTML += `<button type="button" class="btn btn-custom btn-custom-white d-block mb-2 fw-bold rounded-21 mx-auto"
+        data-value = "${difficulty}"
+        >${difficulty}</button>`;
+    });
+
+    mainCardHTML += `</div>`;
+
+    mainCard.innerHTML = mainCardHTML;
+}
+
+renderDifficulties();
 
 // difficulty.addEventListener
 // upon selection of a difficulty with a button, send that difficulty to the callAPI function
@@ -47,3 +83,4 @@ const difficultyArr = [
 // event listener on question buttons to select an answer
 
 // event listener on submit button to submit answer, show correct answer, compare to selected answer and save to historyArr as a new question along with whether the question was selected properly or not.
+
